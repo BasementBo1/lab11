d9cbc2fd8c815928c4681103c259af6acfab0ca9
Alex Sun <a1xsun.617@gmail.com>
2024-12-12 18:03:08 -0500

    resolved issue

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
 def odds_sum(L):
-    """Return the sum of the odd elements of L."""
+    """
+    Return the sum of the odd elements of L.
+    """
     return sum([i for i in L if i % 2 != 0])
 
 def has_letter_cases(s: str):
     """
-    Return True if and only if s contains at least one lowercase letter and at 
-    least one uppercase letter.
+    Return True if and only if s contains at least one lowercase letter and at least one uppercase letter.
     """
     return not s.isupper() and not s.islower()
 
 def find_lowercase_vowel(msg: str):
     """
-    Return the index of the first lowercase vowel (a, e, i, o, u) in msg, 
-    or the length of msg if it does not contain any lowercase vowels.
+    Return the index of the first lowercase vowel (a, e, i, o, u) in msg, or the length of msg if it does not contain any lowercase vowels.
     """
     for index, char in enumerate(msg):
         if char in 'aeiou':
             return index
     return len(msg)
-<<<<<<< HEAD
-"# New change for testing webhook" 
-=======
 
+# Combined changes
+# New change for testing webhook
 def auto_build():
     """
     This function is only for Jenkins auto build changes
     """
     print("If you see this message it means webhook is working")
->>>>>>> c0367356292fec35ba975031502b634b37485267
